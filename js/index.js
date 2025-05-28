e8b8f10a3d9c28476af5f12a840686f932a36060
Aliaksei Karzhou <alexius90e@gmail.com>
2025-05-28 16:51:07 +0300

    feat: added tariffs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+const tariffsTogglerButtons = document.querySelectorAll('.tariffs__toggler-button');
+const tariffsItems = document.querySelectorAll('.tariffs__cards-item');
+
+tariffsTogglerButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const targetId = event.currentTarget.dataset.target;
+    const targetTariffsItem = document.getElementById(targetId);
+
+    console.log(targetTariffsItem);
+
+    if (targetTariffsItem) {
+      tariffsTogglerButtons.forEach((button) => button.classList.remove('active'));
+      event.currentTarget.classList.add('active');
+
+      tariffsItems.forEach((item) => item.classList.remove('active'));
+      targetTariffsItem.classList.add('active');
+    }
+  });
+});
+
 const tariffsFaqMoreEl = document.querySelector('.tariffs-faq__more');
 const tariffsFaqItemEls = document.querySelectorAll('.tariffs-faq__item');
 
